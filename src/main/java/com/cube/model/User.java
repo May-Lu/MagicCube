ba7622a0ed6f6eeb29374f7c314f96728c79f4c8
maylu <1258680185@qq.com>
2018-06-05 21:34:39 +0800

    model——User

---
+++ src/main/java/com/cube/model/User.java
@@ -0,0 +1,63 @@
+package com.cube.model;
+
+//user表
+public class User {
+	private int id;
+	private String user_name;
+	private String sex;
+	private String tel;
+	private String card_id;
+	private String email;
+	private String password;
+	private String role_type;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getCard_id() {
+		return card_id;
+	}
+	public void setCard_id(String card_id) {
+		this.card_id = card_id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole_type() {
+		return role_type;
+	}
+	public void setRole_type(String role_type) {
+		this.role_type = role_type;
+	}
+
+}
