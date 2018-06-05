6dc4fce14ab47f6e22ed3522e58ce04689492291
maylu <1258680185@qq.com>
2018-06-05 21:30:46 +0800

    UUID

---
+++ src/main/java/com/cube/common/UUIDUtil.java
@@ -0,0 +1,12 @@
+package com.cube.common;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+	
+	public static String UUIDGenerator(){
+		String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+		return uuid;
+	}
+
+}
