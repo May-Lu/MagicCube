6f339ac2ecf488b3d52e2ae123494fbcfab5db0e
maylu <1258680185@qq.com>
2018-06-06 17:24:27 +0800

    添加toString方法

--- src/main/java/com/cube/model/User.java
+++ src/main/java/com/cube/model/User.java
@@ -59,5 +59,43 @@ public class User {
 	public void setRole_type(String role_type) {
 		this.role_type = role_type;
 	}
+	
+	/**
+	 * 无参构造器
+	 */
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * 有参构造器
+	 * @param id
+	 * @param user_name
+	 * @param sex
+	 * @param tel
+	 * @param card_id
+	 * @param email
+	 * @param password
+	 * @param role_type
+	 */
+	public User(int id, String user_name, String sex, String tel, String card_id, String email, String password,
+			String role_type) {
+		super();
+		this.id = id;
+		this.user_name = user_name;
+		this.sex = sex;
+		this.tel = tel;
+		this.card_id = card_id;
+		this.email = email;
+		this.password = password;
+		this.role_type = role_type;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", user_name=" + user_name + ", sex=" + sex + ", tel=" + tel + ", card_id=" + card_id
+				+ ", email=" + email + ", password=" + password + ", role_type=" + role_type + "]";
+	}
+	
+	
 
 }
